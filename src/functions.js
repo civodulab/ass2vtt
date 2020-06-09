82002972cfe5d09272e668813a7b90e80284d2a3
Ludovic Bal <ludovic.bal@reseau-canope.fr>
2020-06-09 10:32:16 +0200

    accolade

--- src/functions.js
+++ src/functions.js
@@ -15,9 +15,8 @@ const re_accolade_tiret = /\}-/g;
 const re_softbreak = /\\n/g; // There's no equivalent function in WebVTT.
 const re_hardspace = /\\h/g; // Replace with &nbsp;
 
-
 // prototype
-Number.prototype.toVTTtime = function() {
+Number.prototype.toVTTtime = function () {
   let time = new Date("1995-12-17T00:00:00");
   time.setMilliseconds(this * 1000);
   let time_tab = [
@@ -37,21 +36,19 @@ Number.prototype.toVTTtime = function() {
   return `${time_tab[0]}:${time_tab[1]}:${time_tab[2]}.${time_tab[3]}`;
 };
 
-String.prototype.hexcolor = function() {
-  return (
-    "#" + this.substring(8, 10) + this.substring(6, 8) + this.substring(4, 6)
-  );
+String.prototype.hexcolor = function () {
+  return "#" + this.substring(8, 10) + this.substring(6, 8) + this.substring(4, 6);
 };
 
-String.prototype.styleA11Y = function(style) {
+String.prototype.styleA11Y = function (style) {
   return "<c." + style + ">" + this + "</c>";
 };
 
-String.prototype.italique = function() {
+String.prototype.italique = function () {
   return "<i>" + this + "</i>";
 };
 
-String.prototype.nbLigne = function() {
+String.prototype.nbLigne = function () {
   return this.match(/[\n]/g).length + 1;
 };
 
@@ -62,19 +59,22 @@ function _options() {
     let srt_config = fs.readFileSync("./.ass2vttrc", "utf8");
     srt_config = srt_config && JSON.parse(srt_config);
     styleOutClassic =
-      (srt_config.styleOutClassic && srt_config.styleOutClassic) ||
-      styleOutClassic;
+      (srt_config.styleOutClassic && srt_config.styleOutClassic) || styleOutClassic;
     styleItalicClassic =
       (srt_config.styleItalicClassic && srt_config.styleItalicClassic) ||
       styleItalicClassic;
     styleToVtt = (srt_config.styleToVtt && srt_config.styleToVtt) || styleToVtt;
     integrerStyle =
       (srt_config.integrerStyle && srt_config.integrerStyle) || integrerStyle;
-    extensionA11Y =
-      (srt_config.extensionA11Y && srt_config.extensionA11Y) || extensionA11Y;
+      if (srt_config.extensionA11Y === "") {
+      extensionA11Y = "";
+    } else {
+     extensionA11Y =
+       (srt_config.extensionA11Y && srt_config.extensionA11Y) || extensionA11Y;
+    }
+
     extensionClassic =
-      (srt_config.extensionClassic && srt_config.extensionClassic) ||
-      extensionClassic;
+      (srt_config.extensionClassic && srt_config.extensionClassic) || extensionClassic;
   }
 }
 
@@ -113,7 +113,7 @@ function _position(tc, tags, TV, multiline = false) {
 function _ecritureStyle(parse) {
   let style = [];
   if (integrerStyle) {
-    parse.styles.style.forEach(s => {
+    parse.styles.style.forEach((s) => {
       s[0] = (styleToVtt[s[0]] && styleToVtt[s[0]]) || s[0];
       style.push("STYLE");
       style.push("::cue(." + s[0] + "){");
@@ -137,21 +137,23 @@ function _quadratins(txt) {
   txt = txt.replace(/\\N/g, "\n");
   return txt;
 }
-function _Ita(txt){
+function _Ita(txt) {
   return txt.replace(/{\\i1}/g, "<i>").replace(/{\\i0}/g, "</i>");
 }
-
+function _accoladePos(txt) {
+  return txt.replace(/{[^}]+}/g, "");
+}
 
 module.exports = {
   extensionA11Y: extensionA11Y,
   extensionClassic: extensionClassic,
-  writeFile: function(parse) {
+  writeFile: function (parse) {
     _options();
     let erreurLigneClassic = [];
 
     const TV = {
       width: parse.info.PlayResX,
-      height: parse.info.PlayResY
+      height: parse.info.PlayResY,
     };
     let styles = _ecritureStyle(parse);
     let k = 0;
@@ -169,7 +171,8 @@ module.exports = {
 
       let multiline = d.Text.parsed[0].text.indexOf("\\N") > -1;
       let txt = _quadratins(d.Text.raw);
-      txt=_Ita(txt);
+      txt = _Ita(txt);
+      txt = _accoladePos(txt);
 
       if (
         styleOutClassic.indexOf(d.Style) === -1 &&
@@ -177,7 +180,7 @@ module.exports = {
       ) {
         // txt = txt.replace(/\*/g, "");
         // txt = (txt.substring(0, 1) === "–") && txt.replace(/–/, "") || txt;
-      
+
         // si texte superposé
         if (
           d.Start < end_avant &&
@@ -187,37 +190,36 @@ module.exports = {
           let lf = file_classic.length;
           let index_tc = lf - 3;
           let index_txt = lf - 2;
-          let tc_classique =
-            file_classic[index_tc].split(" --> ")[0] + " --> " + end;
+          let tc_classique = file_classic[index_tc].split(" --> ")[0] + " --> " + end;
           let txt_avant = file_classic[index_txt] + "\n" + txt;
           file_classic.splice(index_tc, 1, tc_classique);
           file_classic.splice(index_txt, 1, txt_avant);
           if (txt_avant.nbLigne() > 2) {
             console.log(
               erreurLigneClassic.findIndex(
-                x => x["sous-titre"] === file_classic[lf - 4]
+                (x) => x["sous-titre"] === file_classic[lf - 4]
               )
             );
             if (
               erreurLigneClassic.findIndex(
-                x => x["sous-titre"] === file_classic[lf - 4]
-              )!==-1
+                (x) => x["sous-titre"] === file_classic[lf - 4]
+              ) !== -1
             ) {
               erreurLigneClassic.pop();
             }
             erreurLigneClassic.push({
               "sous-titre": file_classic[lf - 4],
-              problème: "Trop de ligne (" + txt_avant.nbLigne() + ")"
+              problème: "Trop de ligne (" + txt_avant.nbLigne() + ")",
             });
           }
           k += 1;
         } else {
-           let txt_clas =
-              (styleItalicClassic &&
-                (styleItalicClassic.indexOf(d.Style) !== -1 ||
-                  styleItalicClassic.indexOf(Style) !== -1) &&
-                txt.italique()) ||
-              txt;
+          let txt_clas =
+            (styleItalicClassic &&
+              (styleItalicClassic.indexOf(d.Style) !== -1 ||
+                styleItalicClassic.indexOf(Style) !== -1) &&
+              txt.italique()) ||
+            txt;
           file_classic.push(i + 1 - k);
           file_classic.push(tc);
           file_classic.push(txt_clas);
@@ -238,34 +240,30 @@ module.exports = {
     return {
       _classic: file_classic.join("\n"),
       _a11y: file_a11y.join("\n"),
-      erreurLigneClassic: erreurLigneClassic
+      erreurLigneClassic: erreurLigneClassic,
     };
   },
-  init: function() {
+  init: function () {
     _options();
     let fichierAss = [];
     let chemin = process.cwd();
     const rcPath = path.format({
       dir: chemin,
-      base: ".ass2vttrc"
+      base: ".ass2vttrc",
     });
-    fs.readdir(chemin, function(err, items) {
-      items.forEach(f => {
+    fs.readdir(chemin, function (err, items) {
+      items.forEach((f) => {
         path.extname(f) === ".ass" && fichierAss.push(path.join(chemin, f));
       });
       if (fichierAss.length === 0) {
         monLog.error("Vous n'avez pas de fichiers ass dans votre répertoire");
         return;
       }
-      fs.writeFileSync(
-        rcPath,
-        JSON.stringify(_writeInit(fichierAss), null, 2),
-        "utf8"
-      );
+      fs.writeFileSync(rcPath, JSON.stringify(_writeInit(fichierAss), null, 2), "utf8");
       monLog.log("ass2vtt", rcPath, "généré");
     });
   },
-  recupProcessArgv: function(argv) {
+  recupProcessArgv: function (argv) {
     let erreur = false;
     let file_in = "";
     switch (argv._.length) {
@@ -283,24 +281,22 @@ module.exports = {
         break;
 
       default:
-        monLog.error(
-          "Vous ne pouvez entrer qu'un seul argument (File_in.ass)."
-        );
+        monLog.error("Vous ne pouvez entrer qu'un seul argument (File_in.ass).");
         erreur = true;
         break;
     }
     return {
       file_in: file_in,
-      erreur: erreur
+      erreur: erreur,
     };
-  }
+  },
 };
 
 function _writeInit(ass) {
-  ass.forEach(f => {
+  ass.forEach((f) => {
     const data = fs.readFileSync(f, "utf8");
     const parse = parseASS(data);
-    parse.styles.style.forEach(s => {
+    parse.styles.style.forEach((s) => {
       !styleToVtt[s[0]] && (styleToVtt[s[0]] = s[0]);
     });
   });
@@ -311,6 +307,6 @@ function _writeInit(ass) {
     styleItalicClassic: styleItalicClassic,
     integrerStyle: integrerStyle,
     extensionA11Y: extensionA11Y,
-    extensionClassic: extensionClassic
+    extensionClassic: extensionClassic,
   };
 }
